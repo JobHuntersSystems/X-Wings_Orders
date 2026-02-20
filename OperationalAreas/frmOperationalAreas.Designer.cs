2d225e040ad9c16bf4d3314e9abc0ebfc288b659
MartinaCanamares <martina.canamares@sarria.salesians.cat>
2026-02-20 20:53:33 +0100

    formulario opAreas sirve

---
+++ OperationalAreas/frmOperationalAreas.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace OperationalAreas
+{
+    partial class frmOperationalAreas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblDesc = new System.Windows.Forms.Label();
+            this.lblCode = new System.Windows.Forms.Label();
+            this.txtDesc = new System.Windows.Forms.TextBox();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.logsTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+
+            // 
+            // lblDesc
+            // 
+            this.lblDesc.AutoSize = true;
+            this.lblDesc.Location = new System.Drawing.Point(78, 235);
+            this.lblDesc.Name = "lblDesc";
+            this.lblDesc.Size = new System.Drawing.Size(115, 24);
+            this.lblDesc.TabIndex = 18;
+            this.lblDesc.Text = "Description";
+            // 
+            // lblCode
+            // 
+            this.lblCode.AutoSize = true;
+            this.lblCode.Location = new System.Drawing.Point(135, 175);
+            this.lblCode.Name = "lblCode";
+            this.lblCode.Size = new System.Drawing.Size(60, 24);
+            this.lblCode.TabIndex = 16;
+            this.lblCode.Text = "Code";
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.Location = new System.Drawing.Point(255, 232);
+            this.txtDesc.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.Size = new System.Drawing.Size(436, 29);
+            this.txtDesc.TabIndex = 12;
+            this.txtDesc.Tag = "DescOperationalArea";
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(255, 170);
+            this.txtCode.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtCode.MaxLength = 12;
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(233, 29);
+            this.txtCode.TabIndex = 13;
+            this.txtCode.Tag = "CodeOperationalArea";
+            // 
+            // logsTimer
+            // 
+            this.logsTimer.Interval = 2000;
+            this.logsTimer.Tick += new System.EventHandler(this.logsTimer_Tick);
+            // 
+            // frmOperationalAreas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+
+            this.Controls.Add(this.lblDesc);
+            this.Controls.Add(this.lblCode);
+            this.Controls.Add(this.txtCode);
+            this.Controls.Add(this.txtDesc);
+            this.Name = "frmOperationalAreas";
+            this.Load += new System.EventHandler(this.frmOperationalAreas_Load);
+            this.Controls.SetChildIndex(this.txtDesc, 0);
+            this.Controls.SetChildIndex(this.txtCode, 0);
+            this.Controls.SetChildIndex(this.lblCode, 0);
+            this.Controls.SetChildIndex(this.lblDesc, 0);
+            this.Controls.SetChildIndex(this.btnUpdate, 0);
+            this.Controls.SetChildIndex(this.btnCreate, 0);
+            this.Controls.SetChildIndex(this.lblTitle, 0);
+            this.Controls.SetChildIndex(this.lblLog, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblDesc;
+        private System.Windows.Forms.Label lblCode;
+        private System.Windows.Forms.TextBox txtDesc;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Timer logsTimer;
+    }
+}
---
+++ OperationalAreas/frmOperationalAreas.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace OperationalAreas
+{
+    public partial class frmOperationalAreas : FormsDesigner.frmBase
+    {
+        OperationalAreasEntities db;
+        List<OperationalAreas> oa;
+
+        BindingSource bs;
+        public frmOperationalAreas(string tableName)
+        {
+            InitializeComponent();
+            base._tableName = tableName;
+            db = new OperationalAreasEntities();
+            bs = new BindingSource();
+        }        
+
+        bool isNew = false;
+        Color originalForeColor;
+
+        private void ReadData()
+        {
+            oa = db.OperationalAreas.ToList();
+
+            bs.DataSource = oa;
+
+            dgvBase.DataSource = bs;
+            Bind();
+        }
+
+        private void dgvConfiguration()
+        {
+            dgvBase.Columns["idOperationalArea"].Visible = false;
+            dgvBase.Columns["CodeOperationalArea"].HeaderText = "Code";
+            dgvBase.Columns["DescOperationalArea"].HeaderText = "Description";
+        }
+        private void Bind()
+        {
+            foreach (TextBox txt in this.Controls.OfType<TextBox>())
+            {
+                txt.DataBindings.Clear();
+                txt.Clear();
+                txt.DataBindings.Add("Text", bs, txt.Tag.ToString(), true, DataSourceUpdateMode.OnValidation);
+            }
+        }
+        private void NoBind()
+        {
+            foreach (TextBox txt in this.Controls.OfType<TextBox>())
+            {
+                txt.Clear();
+                txt.DataBindings.Clear();
+            }
+        }
+
+        private void dgvOperationalArea_UserDeletingRow(object sender, DataGridViewRowCancelEventArgs e)
+        {
+
+            var ent = e.Row.DataBoundItem as OperationalAreas;
+            if (ent == null) return;
+
+            DialogResult dr = MessageBox.Show("Are you sure?", "Confirmr", MessageBoxButtons.YesNo);
+
+            if (dr == DialogResult.Yes)
+            {
+                if (db.Entry(ent).State != EntityState.Added)
+                {
+                    db.Entry(ent).State = EntityState.Deleted;
+                }
+                db.SaveChanges();
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void frmOperationalAreas_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                ReadData();
+                dgvConfiguration();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bs.EndEdit();
+                if (isNew && txtCode.Text.Trim().Length != 0)
+                {
+                    OperationalAreas opar = new OperationalAreas()
+                    {
+                        CodeOperationalArea = txtCode.Text,
+                        DescOperationalArea = txtDesc.Text
+                    };
+                    db.OperationalAreas.Add(opar);
+                }
+                db.SaveChanges();
+
+                lblLog.Visible = true;
+                lblLog.Text = "Registers updated successfully";
+                logsTimer.Start(); 
+
+                ReadData();
+                isNew = false;
+
+                btnCreate.Text = "Create";
+                btnCreate.ForeColor = originalForeColor;
+
+            }
+            catch (System.Data.Entity.Validation.DbEntityValidationException ex)
+            {
+                lblLog.Visible = true;
+                lblLog.Text = "Error:" + ex.Message;
+                logsTimer.Start();
+            }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                isNew = true;
+                NoBind();
+                txtCode.Focus();
+
+                originalForeColor = btnCreate.ForeColor;
+                btnCreate.Text = "Cancel";
+                btnCreate.ForeColor = Color.Red;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void logsTimer_Tick(object sender, EventArgs e)
+        {
+            logsTimer.Stop();
+            lblLog.Text = "";
+            lblLog.Visible = false;
+            logsTimer.Dispose();
+        }
+    }
+}
